27fd4e913d76b2b98c6eb2126115347350db128b
sayalipawade <sayalipawade05@gmail.com>
2020-03-21 22:46:24 +0530

    Added:Flipping coins and storing the combination of triplet and calculating the percenatge

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -66,6 +66,68 @@ echo "${!dictionary[@]}"
 echo "${dictionary[@]}"
 		
 				
+#flipping a coin and store the Triplet combination
+HHH=0
+TTT=0
+HHT=0
+HTH=0
+THH=0
+THT=0
+TTH=0
+HTT=0
+for (( i=0;i<10;i++))
+do
+   coin1=$((RANDOM%2))
+   coin2=$((RANDOM%2))
+	coin3=$((RANDOM%2))
+   if [[ $coin1 -eq 0 &&  $coin2 -eq 0 && $coin3 -eq 0 ]]
+   then
+      ((HHH++))
+   elif [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 1 ]]
+   then
+      ((TTT++))
+   elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 1 ]]
+   then
+      ((HHT++))
+   elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 0 ]]
+   then
+      ((HTH++))
+	elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 0 ]]
+   then
+      ((THH++))
+	elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 1 ]]
+   then
+      ((THT++))
+	elif [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 0 ]]
+   then
+      ((TTH++))
+	else
+      ((HTT++))
+	fi
+done
+dictionary[0]=$HHH
+dictionary[1]=$TTT
+dictionary[2]=$HHT
+dictionary[3]=$HTH
+dictionary[4]=$THH
+dictionary[5]=$THT
+dictionary[6]=$TTH
+dictionary[7]=$HTT
+echo "${!dictionary[@]}"
+echo "${dictionary[@]}"
+
+#calculating the percentage of triplet combination 
+percentage1=$((($HHH*100)/10))
+percentage2=$((($TTT*100)/10))
+percentage3=$((($HHT*100)/10))
+percentage4=$((($HTH*100)/10))
+percentage5=$((($THH*100)/10))
+percentage6=$((($THT*100)/10))
+percentage7=$((($TTH*100)/10))
+percentage8=$((($HTT*100)/10))
+
+
+
 
 
 
